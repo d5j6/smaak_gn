e2ce8903215ad583f5cc9c845609b07324ee07dc
Stijn Delaruelle <stijndelaruelle@gmail.com>
2017-06-14 00:25:51 +0200

    Added new demo scene + SLATE animation trigger

---
+++ Assets/ParadoxNotion/SLATE Resources/Extensions/PlayAnimation.cs
@@ -0,0 +1,30 @@
+﻿using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Stijn: custom extention
+namespace Slate.ActionClips.NodeCanvas
+{
+    [Category("Animation")]
+    [Description("Change the attached animator to the requested state.")]
+    public class PlayAnimation : ActorActionClip<Animator>
+    {
+        [SerializeField]
+        public string m_AnimationName;
+
+        public override string info
+        {
+            get { return string.Format("Play animation"); }
+        }
+
+        protected override void OnEnter()
+        {
+            if (Application.isPlaying)
+            {
+                actor.Play(m_AnimationName);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    [Header("Horizontal Rotation")]
+    [Space(5)]
+    [SerializeField]
+    private float m_HorizontalRotationSpeed;
+    private float m_HorizontalRotationTimer;
+
+    [Space(10)]
+    [Header("Vertical Movement")]
+    [Space(5)]
+    [SerializeField]
+    private float m_VerticalMovementDistance;
+
+    [SerializeField]
+    private float m_VerticalMovementSpeed;
+    private float m_VerticalMovementTimer;
+
+    private Vector3 m_OriginalPosition;
+    private Quaternion m_OriginalRotation;
+
+    private void Awake()
+    {
+        //Create a copy of the original transform values.
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+
+        m_OriginalPosition = new Vector3(pos.x, pos.y, pos.z);
+        m_OriginalRotation = new Quaternion(rot.x, rot.y, rot.z, rot.w);
+    }
+
+    private void Update()
+    {
+        float sin = Mathf.Sin(m_VerticalMovementTimer);
+
+        transform.position = m_OriginalPosition + new Vector3(0.0f, sin * m_VerticalMovementDistance, 0.0f);
+
+        transform.rotation = m_OriginalRotation * Quaternion.Euler(0.0f, m_HorizontalRotationTimer * 360.0f, 0.0f);
+
+        //Increase timers
+        m_VerticalMovementTimer += m_VerticalMovementSpeed * Time.deltaTime;
+        if (m_VerticalMovementTimer > (Mathf.PI * 2)) { m_VerticalMovementTimer -= Mathf.PI * 2; }
+
+        m_HorizontalRotationTimer += m_HorizontalRotationSpeed * Time.deltaTime;
+        if (m_HorizontalRotationTimer > 1.0f) { m_HorizontalRotationTimer -= 1.0f; }
+    }
+}
